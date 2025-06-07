329f1eb043f34c733a3cdc3fe112a954d4ba417b
pavankumar.m <pavankumar.m@zohocorp.com>
2025-06-07 12:57:37 +0530

    Basic get api status code and response body assertions using HamCrest

---
+++ src/test/java/userManagement/GetUsers.java
@@ -0,0 +1,40 @@
+package userManagement;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class GetUsers {
+
+    @Test
+    public void getUsersData() {
+
+        given()
+        .when()
+             .get("https://reqres.in/api/users?page=2")
+        .then()
+             .statusCode(200);
+
+    }
+
+    @Test
+    public void validateGetResponseBody() {
+
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+
+        given()
+        .when()
+             .get("/todos/1")
+        .then()
+                .statusCode(200)
+                .body(not(emptyString()))
+                .body("title", equalTo("delectus aut autem"))
+                .body("userId", equalTo(1));
+
+    }
+
+
+
+}
